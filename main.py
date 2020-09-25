b1a6f2348c5b7981601c1c83a3d538320667b997
HunterDietrich <70781742+HunterDietrich@users.noreply.github.com>
2020-09-25 23:20:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# Review
+
+# Variables
+number = 0
+
+# Loops
+for i in range(3):
+    number += i
+    print(number)
+
+number = 0
+
+while number < 5:
+    number += 1
+    print(f"number is {number}")
+
+    if number % 2 == 0:
+        print(f"{number} is even")
+    if number % 2 != 0:
+        print(f"{number} is odd")
+
